9975b5ed225aab3639d17d47f99542d84c294d9c
Sagarnil Das <sagarnildass@gmail.com>
2023-02-21 03:02:31 +0530

    first commit

---
+++ shareme_frontend/src/components/Login.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+import { client } from "../client";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const responseGoogle = (response) => {
+    const decode = jwt_decode(response.credential);
+    localStorage.setItem("user", JSON.stringify(decode));
+    const { name, sub, picture } = decode;
+    console.log(decode);
+    const doc = {
+      _id: sub,
+      _type: "user",
+      userName: name,
+      image: picture,
+    };
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
+  };
+  return (
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+      <div className="flex justify-start items-center flex-col h-screen">
+        <div className="relative w-full h-full">
+          <video
+            src={shareVideo}
+            type="video/mp4"
+            loop
+            controls={false}
+            muted
+            autoPlay
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+            <div className="p-5">
+              <img src={logo} width="130px" alt="logo" />
+            </div>
+            <div className="shadow-2xl">
+              <GoogleLogin
+                onSuccess={(response) => responseGoogle(response)}
+                onError={() => console.log("Error")}
+                cookiePolicy="single_host_origin"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  );
+};
+
+export default Login;
---
+++ shareme_frontend/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+
+import "./index.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
